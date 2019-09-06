80bbb75e51eb6b1733618cc96dfdf1cbaa329753
mpm11011 <markuscicero5@gmail.com>
2019-09-06 17:31:48 -0400

    get_owned_games implemented, removed incorrect methods.

--- plugin.py
+++ plugin.py
@@ -2,6 +2,8 @@ import sys
 import os
 import json
 import subprocess
+import re
+import requests
 
 from galaxy.api.plugin import Plugin, create_and_run_plugin
 from galaxy.api.consts import Platform, LocalGameState
@@ -15,42 +17,75 @@ class RPCS3Plugin(Plugin):
             __version__,
             reader,
             writer,
-            token
-        )
+            token)
+
+    def tick(self):
+        pass
         
     ### Authentication ###
         
     async def authenticate(self, stored_credentials=None):
-        pass
+        return self.handle_auth()
 
     async def pass_login_credentials(self, step, credentials, cookies):
-        pass
+        return self.handle_auth()
 
-    ### Platform ###
-
-    async def launch_platform_client(self):
+    def handle_auth(self):
 
         with open('config.json') as config_file:
             config = json.load(config_file)
-            rpcs3_exe = config['rpcs3']['home'] + '/' + config['rpcs3']['exe']
-            info = subprocess.STARTUPINFO(dwFlags=1, wShowWindow=0)
-            subprocess.Popen(rpcs3_exe, startupinfo=info)
 
-    async def shutdown_platform_client(self):
-        pass
+        username_filename = json2path(
+            config['rpcs3']['home'], 
+            config['user']['home'], 
+            config['user']['namefile'])
+
+        with open(username_filename) as username_file:
+            username = username_file.read()
+
+        user_data = {}
+        user_data["username"] = username
+        self.store_credentials(user_data)
+        return Authentication("rpcs3_user", user_data["username"])
 
     ### Library Management ###
 
     async def get_owned_games(self):
-        pass
+
+        url = 'https://rpcs3.net/compatibility?api=v1&g='
+        owned_games = []
+
+        with open('config.json') as config_file:
+            config = json.load(config_file)
+
+        games_filename = json2path(
+            config['rpcs3']['home'], 
+            config['rpcs3']['games'])
+
+        with open(games_filename) as games_file: 
+            game_ids = re.findall('\w+(?=: .*)', games_file.read())
+
+
+        for game_id in game_ids:
+            page = requests.get(url + game_id)
+
+            game = json.loads(page.text)['results'][game_id]
+
+            owned_games.append(Game(
+                game_id, 
+                game['title'], 
+                None, 
+                LicenseInfo(LicenseType.SinglePurchase, None)))
+
+        return owned_games
 
     async def get_local_games(self):
-        pass
+        return []
 
     ### Game Management ###
 
     async def launch_game(self, game_id):
-        pass
+        return
 
     async def install_game(self, game_id):
         pass
@@ -70,6 +105,12 @@ class RPCS3Plugin(Plugin):
 
     async def get_friends(self):
         pass
+
+    ### Helpers and Miscellaneous ###
+
+    def json2path(path, *paths):
+        return os.path.normpath(os.path.join(path, *paths))
+
 
 def main():
     create_and_run_plugin(RPCS3Plugin, sys.argv)
